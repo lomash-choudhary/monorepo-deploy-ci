78af855578dad8e6d685dcc4a2ccd6d4284e5a9a
lomash-choudhary <lomashchoudhary9812@gmail.com>
2025-02-18 21:21:14 +0530

    init

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,37 @@
+import express, {type Request, type Response} from "express"
+import { client } from "@repo/db/client"
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).json(
+        {
+            message:"server listening"
+        }
+    )
+})
+//@ts-ignore
+app.post("/user", async (req: Request, res: Response) => {
+    const username = Math.floor(Math.random() * 1000).toString();
+    const password = Math.floor(Math.random() * 1000).toString();
+
+    const user = await client.user.create(
+        {
+            data:{
+                username: username,
+                password: password
+            }
+        }
+    )
+
+    return res.status(200).json(
+        {
+            user
+        }
+    )
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port: ${port}`)
+})
